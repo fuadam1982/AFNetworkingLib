9d6ee1207b71d122991de1c44f5c0f3e39cb6b0f
hell-lucifer <lucifer@hell-luciferdeMacBook-Pro.local>
2018-07-13 18:10:44 +0800

    repo init

---
+++ Example/Pods/Target Support Files/Pods-AFNetworkingLib_Example/Pods-AFNetworkingLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AFNetworkingLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AFNetworkingLib_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-AFNetworkingLib_Tests/Pods-AFNetworkingLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AFNetworkingLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AFNetworkingLib_Tests
+@end
